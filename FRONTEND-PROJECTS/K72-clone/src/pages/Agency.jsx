a0a3e6e04423ec6965ea214ef1fb5505c739e693
Harshvardhan Puranik <hvp0926@gmail.com>
2026-03-14 16:57:50 +0530

    logic

--- FRONTEND-PROJECTS/K72-clone/src/pages/Agency.jsx
+++ FRONTEND-PROJECTS/K72-clone/src/pages/Agency.jsx
@@ -4,7 +4,19 @@ import React, { useRef } from 'react'
 import ScrollTrigger from 'gsap/src/ScrollTrigger'
 
 const Agency = () => {
+
+  const imgArray = ['https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7',
+                    'https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64',
+                    'https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991',
+                    'https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a',
+                    'https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6',
+                    'https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847',
+                    'https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2',
+                    'https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6'
+  ]
+
   const imageDivRef = useRef(null)
+  const imageRef = useRef(null)
   gsap.registerPlugin(ScrollTrigger)
   useGSAP(
     function(){
@@ -15,7 +27,17 @@ const Agency = () => {
           start:'top 36%',
           end:'top -100%',
           scrub: true,
-          pin: true
+          pin: true,
+          onUpdate: (elem) => {
+            let imageIndex;
+            if(elem.progress<1){
+              imageIndex = Math.floor(elem.progress*imgArray.length)
+            }else{
+              imageIndex = imgArray.length-1
+            }
+              
+              imageRef.current.src = imgArray[imageIndex]
+          }
         }
       })
     }
@@ -24,7 +46,7 @@ const Agency = () => {
     <div>
       <div className='section1'>
         <div ref={imageDivRef} className='absolute  h-[20vw] w-[15vw]  top-50 left-[30vw] rounded-3xl overflow-hidden'>
-          <img className='h-full w-full object-cover' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
+          <img ref={imageRef} className='h-full w-full object-cover' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
         </div>
         <div className='font-[font2] relative'>
           <div className='mt-[55vh]'>
